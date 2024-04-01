d48792aab86de09dd337e9a0ff2c15a5cfdeabb9
LuizFelipeHoffmann <144742259+LuizFelipeHoffmann@users.noreply.github.com>
2024-03-31 23:24:19 -0300

    Pizza.java

---
+++ Pizzaria/Pizza.java
@@ -0,0 +1,17 @@
+class Pizza {
+    private String nome;
+    private double valor;
+
+    public Pizza(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
